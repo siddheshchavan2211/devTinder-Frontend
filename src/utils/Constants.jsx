ff0cba63ef0b8406b58b788df54d649eeed37362
siddheshchavan2211 <siddheshchavan02@gmail.com>
2025-01-23 14:39:36 +0530

    toggle in port

--- src/utils/Constants.jsx
+++ src/utils/Constants.jsx
@@ -1,3 +1,4 @@
 export const TinderBg =
   "https://tinder.com/static/build/8ad4e4299ef5e377d2ef00ba5c94c44c.webp";
-export const ApiUrl = "/api";
+export const ApiUrl =
+  location.hostname === "localhost" ? "http://localhost:8000" : "/api";
